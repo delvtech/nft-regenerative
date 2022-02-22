5e76127b6618bb83694ea877767b0a78c52b8fb5
mihai6 <mihai@element.fi>
2022-02-21 18:17:45 -0700

    convert to named functions

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ let decodedHash = [];
 const Exists = new Map();
 
 
-const addRarity = _str => {
+function addRarity(_str) {
   let itemRarity;
 
   rarity.forEach((r) => {
@@ -36,17 +36,17 @@ const addRarity = _str => {
   });
 
   return itemRarity;
-};
+}
 
-const cleanName = _str => {
+function cleanName(_str) {
   let name = _str.slice(0, -4);
   rarity.forEach((r) => {
     name = name.replace(r.key, "");
   });
   return name;
-};
+}
 
-const getElements = path => {
+function getElements(path) {
   return fs
     .readdirSync(path)
     .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
@@ -58,9 +58,9 @@ const getElements = path => {
         rarity: addRarity(i),
       };
     });
-};
+}
 
-const layersSetup = layersOrder => {
+function layersSetup(layersOrder) {
   var layers = layersOrder.map((layerObj, index) => ({
     id: index,
     name: layerObj.name,
@@ -72,30 +72,30 @@ const layersSetup = layersOrder => {
   }));
   var combinations = 0;
   layers.forEach(layer => {
-    layer.numElements = layer.elements.length
-    if (combinations ==0) {
+    layer.numElements = layer.elements.length;
+    if (combinations == 0) {
       combinations = layer.numElements;
     } else {
       combinations = combinations * layer.numElements;
     }
-  })
-  console.log('read in ' + layers.length + ' layers with ' + numberWithCommas(combinations) + ' unique combinations')
+  });
+  console.log('read in ' + layers.length + ' layers with ' + numberWithCommas(combinations) + ' unique combinations');
 
   return layers;
-};
+}
 
-const buildSetup = () => {
+function buildSetup() {
   if (fs.existsSync(buildDir)) {
     fs.rmSync(buildDir, { recursive: true });
   }
-  else {fs.mkdirSync(buildDir)};
-};
+  else { fs.mkdirSync(buildDir); };
+}
 
-const saveLayer = (_canvas, _edition) => {
+function saveLayer(_canvas, _edition) {
   fs.writeFileSync(`${buildDir}/${_edition}.png`, _canvas.toBuffer("image/png"));
-};
+}
 
-const addMetadata = _edition => {
+function addMetadata(_edition) {
   let dateTime = Date.now();
   let tempMetadata = {
     hash: hash.join(""),
@@ -108,9 +108,9 @@ const addMetadata = _edition => {
   attributes = [];
   hash = [];
   decodedHash = [];
-};
+}
 
-const addAttributes = (_element, _layer) => {
+function addAttributes(_element, _layer) {
   let tempAttr = {
     id: _element.id,
     layer: _layer.name,
@@ -121,23 +121,23 @@ const addAttributes = (_element, _layer) => {
   hash.push(_layer.id);
   hash.push(_element.id);
   decodedHash.push({ [_layer.id]: _element.id });
-};
+}
 
-const drawLayer = async (_layer, _edition, _element) => {
-    if (_element) {
+async function drawLayer(_layer, _edition, _element) {
+  if (_element) {
 
-      const image = await loadImage(`${_layer.location}${_element.fileName}`);
+    const image = await loadImage(`${_layer.location}${_element.fileName}`);
 
-      ctx.drawImage(
-        image,
-        _layer.position.x,
-        _layer.position.y,
-        _layer.size.width,
-        _layer.size.height
-      );
-      saveLayer(canvas, _edition);
+    ctx.drawImage(
+      image,
+      _layer.position.x,
+      _layer.position.y,
+      _layer.size.width,
+      _layer.size.height
+    );
+    saveLayer(canvas, _edition);
   }
-};
+}
 
 const calcRarity = async layers => {
   // pick an asset
@@ -148,50 +148,51 @@ const calcRarity = async layers => {
   return elements;
 }
 
-const createFiles = async (edition,to_draw) => {
+async function createFiles(edition, to_draw) {
   const layers = layersSetup(layersOrder);
   let numDupes = 0;
-  var startTime = performance.now()
- for (let i = 1; i <= edition; i++) {
-   let rarities = await calcRarity(layers);
-   console.log(rarities);
-   await layers.forEach(async (layer,layerIdx) => { // for each Layer
-    // console.log(i + ' layer:')
-    // console.log(layer)
-    let element = layer.elements[rarities[layerIdx]] ? layer.elements[rarities[layerIdx]] : null;
-    // console.log(element)
-    addAttributes(element, layer);
-    if (to_draw) {await drawLayer(layer, i, element);}
-   });
+  var startTime = performance.now();
+  for (let i = 1; i <= edition; i++) {
+    let rarities = await calcRarity(layers);
+    console.log(rarities);
+    await layers.forEach(async (layer, layerIdx) => {
+      // console.log(i + ' layer:')
+      // console.log(layer)
+      let element = layer.elements[rarities[layerIdx]] ? layer.elements[rarities[layerIdx]] : null;
+      // console.log(element)
+      addAttributes(element, layer);
+      if (to_draw) { await drawLayer(layer, i, element); }
+    });
 
-   // by now it's fully created, so we check for duplicate
-   let key = hash.toString();
-   if (Exists.has(key)) {
-     console.log(
-       `Duplicate creation for edition ${i}. Same as edition ${Exists.get(
-         key
-       )}`
-     );
-     numDupes++;
-     if (numDupes > edition) break; //prevents infinite loop if no more unique items can be created
-     i--;
-   } else {
-     Exists.set(key, i);
-     addMetadata(i);
-     console.log("Created edition " + i + ' at ' + numberWithCommas(i/(performance.now()-startTime)*1000*60) + ' Elfis/minute');
-   }
- }
- console.log('Created ' + edition + ' editions in '+(performance.now()-startTime)/1000+' seconds');
-};
+    // by now it's fully created, so we check for duplicate
+    let key = hash.toString();
+    if (Exists.has(key)) {
+      console.log(
+        `Duplicate creation for edition ${i}. Same as edition ${Exists.get(
+          key
+        )}`
+      );
+      numDupes++;
+      if (numDupes > edition)
+        break; //prevents infinite loop if no more unique items can be created
+      i--;
+    } else {
+      Exists.set(key, i);
+      addMetadata(i);
+      console.log("Created edition " + i + ' at ' + numberWithCommas(i / (performance.now() - startTime) * 1000 * 60) + ' Elfis/minute');
+    }
+  }
+  console.log('Created ' + edition + ' editions in ' + (performance.now() - startTime) / 1000 + ' seconds');
+}
 
-const createMetaData = () => {
+function createMetaData() {
   fs.stat(`${buildDir}/${metDataFile}`, (err) => {
-    if(err == null || err.code === 'ENOENT') {
+    if (err == null || err.code === 'ENOENT') {
       fs.writeFileSync(`${buildDir}/${metDataFile}`, JSON.stringify(metadata, null, 2));
     } else {
-        console.log('Oh no, error: ', err.code);
+      console.log('Oh no, error: ', err.code);
     }
   });
-};
+}
 
 module.exports = { buildSetup, createFiles, createMetaData };
