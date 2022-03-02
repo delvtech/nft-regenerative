d13ab8618dd5bf9a2cfd231562c0aac8b566f709
Mihai6 <mihai@element.fi>
2022-03-01 19:15:56 -0500

    baseline rarities and demo 100

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 const layersOrder = [
-    { name: '00_background', number: 1, allowNone: true },
-    { name: '01_weapons', number: 2, allowNone: true },
-    { name: '02_bodies', number: 12, allowNone: false },
-    { name: '03_clothes', number: 12, allowNone: true },
-    { name: '04_hair', number: 3, allowNone: true },
-    { name: '05_mouth', number: 1, allowNone: true },
-    { name: '06_eyebrows', number: 1, allowNone: true },
-    { name: '07_eyes', number: 1, allowNone: false },
-    { name: '08_hats', number: 3, allowNone: true },
-    { name: '09_eyewear', number: 2, allowNone: true },
-    { name: '10_addons', number: 1, allowNone: true },
+    { name: '00_background', number: 1, allowNone: false },  // TURN THIS OFF LATER: everyone has background
+    { name: '01_weapons', number: 2, allowNone: 80 },
+    { name: '02_bodies', number: 12, allowNone: false },    // everyone has bodies
+    { name: '03_clothes', number: 12, allowNone: 2 },
+    { name: '04_hair', number: 3, allowNone: 25 },
+    { name: '05_mouth', number: 1, allowNone: 2 },
+    { name: '06_eyebrows', number: 1, allowNone: false },
+    { name: '07_eyes', number: 1, allowNone: 1 },       // everyone has eyes
+    { name: '08_hats', number: 3, allowNone: 70 },
+    { name: '09_eyewear', number: 2, allowNone: 52 },
+    { name: '10_addons', number: 1, allowNone: 88 },
  
 ];
   
